47d9b85a01450c99a74fa7feeba6c91b9515026e
Vikash Mishra <131462517+vikashmishra1234@users.noreply.github.com>
2025-02-15 10:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT =process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin:["https://scrape-gpt.vercel.app","http://localhost:5173"],
+    origin:["https://chatgptpdf.vercel.app","http://localhost:5173"],
     methods: ["POST", "GET"],
     credentials: true
   })
